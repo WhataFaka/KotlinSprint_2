893e27e4494fafbd17e89e6ad02f2900a545236d
Your Name <mrmofd@gmail.com>
2023-09-07 18:26:59 +0700

    KS-1-5 Corrected to String.format()

--- src/main/kotlin/lesson1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson1/lesson_1_task_5.kt
@@ -1,14 +1,13 @@
 package lesson1
 
 fun main() {
-
     val timeInSpace: Short = 6480
 
     val hours = timeInSpace / 3600
     val minutes = timeInSpace / 60 % 60
     val seconds = timeInSpace / 3600 / 60
 
-    val result = "0$hours:$minutes:0$seconds;"
+    val result = String.format("0%d:%d:0%d", hours, minutes, seconds)
     println(result)
-
 }
+
