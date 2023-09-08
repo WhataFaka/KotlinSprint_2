5e6873a63aeb07f69faafaf4308a512b9340840e
Your Name <mrmofd@gmail.com>
2023-09-08 23:28:11 +0700

    KS-3-2 brought an entry about a person with an age change to the console

---
+++ src/main/kotlin/lesson3/lesson_3_task_2.kt
@@ -0,0 +1,15 @@
+package lesson3
+
+fun main() {
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    val lastName = "Андреева"
+    val newLastName = "Сидорова"
+
+    val age1 = 20
+    val age2 = 22
+
+    println("$lastName $firstName $middleName, $age1")
+    println("$newLastName $firstName $middleName, $age2")
+
+}
