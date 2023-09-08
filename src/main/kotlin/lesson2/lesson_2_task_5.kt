e4385a3f9a4ca8c001bdc0dd3bcf51d222668412
Your Name <mrmofd@gmail.com>
2023-09-09 03:04:04 +0700

    KS-2-5 stylized and removed unnecessary variable assignment

--- src/main/kotlin/lesson2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson2/lesson_2_task_5.kt
@@ -2,19 +2,13 @@ package lesson2
 
 import kotlin.math.pow
 
-
-
 fun main() {
 
     val contribution = 70_000.0
     val interestRate = 16.7
     val year = 20
-    var amount = contribution
 
-
-
-    amount = contribution * (1 + interestRate / 100).pow(year)
-
+    val amount = contribution * (1 + interestRate / 100).pow(year)
 
     println(String.format("Размер вклада через 20 лет: %.3f", amount))
 }
