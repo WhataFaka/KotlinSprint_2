0b80a790cf057bafc8c9a0af805e39f64a83d4e4
Your Name <mrmofd@gmail.com>
2023-09-08 12:17:08 +0700

    KS-2-3 Fixed calculations

--- src/main/kotlin/lesson2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson2/lesson_2_task_3.kt
@@ -2,19 +2,18 @@ package lesson2
 
 
 fun main() {
-    val departureTime = 9 * 60 + 39
+    val departureTimeInHours = 9
+    val departureTimeInMinutes = 39
     val travelTime = 457
-    val travelTimeInHours = travelTime / 60
 
-    val arrivalTime = calculateArrivalTime(departureTime, travelTime)
-
+    val arrivalTime = calculateArrivalTime(departureTimeInHours, travelTime, departureTimeInMinutes)
 
     println(arrivalTime)
-    println(travelTimeInHours)
+
 }
 
-fun calculateArrivalTime(departureTime: Int, travelTimeInHours: Int): String {
-    val arrivalHour = (departureTime / 100 + travelTimeInHours / 60) % 24
-    val arrivalMinute = (departureTime % 100 * 60 + travelTimeInHours % 60) % 60
+fun calculateArrivalTime(departureTimeInHours: Int, travelTime: Int, departureTimeInMinutes: Int): String {
+    val arrivalHour = (departureTimeInHours + (departureTimeInMinutes + travelTime) / 60) % 24
+    val arrivalMinute = (departureTimeInMinutes % 100 + travelTime % 60) % 60
     return String.format("Arrival time: %02d:%02d", arrivalHour, arrivalMinute)
 }
