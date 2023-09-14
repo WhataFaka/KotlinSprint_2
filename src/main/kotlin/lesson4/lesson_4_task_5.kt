dc5ef5820742abbe4f55f31ddf28aea63d28b52a
Your Name <mrmofd@gmail.com>
2023-09-14 14:09:42 +0700

    KS-4-5 created const variables

--- src/main/kotlin/lesson4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson4/lesson_4_task_5.kt
@@ -7,13 +7,21 @@ fun main() {
     val foodBoxesCount = readln().toInt()
     val isWeatherCondition = readln().toBoolean()
 
-    if ((!isDamage) && (crewCount in 55..70) && (foodBoxesCount > 50) && isWeatherCondition) {
+    if ((isDamage != IS_DAMAGE) && (crewCount in CREW_COUNT_MIN..CREW_COUNT_MAX) && (foodBoxesCount > FOOD_BOXES_COUNT) && (isWeatherCondition ==
+                IS_WEATHER_CONDITION)
+    ) {
         println("Корабль может приступить к долгосрочному плаванию")
     } else {
-        if (((isDamage && crewCount == 70) && foodBoxesCount > 50) && isWeatherCondition) {
+        if ((((isDamage == IS_DAMAGE) && (crewCount == CREW_COUNT_MAX)) && (foodBoxesCount > FOOD_BOXES_COUNT)) && (isWeatherCondition == IS_WEATHER_CONDITION)) {
             println("Корабль может отплыть, несмотря на повреждения")
         } else {
             println("Корабль не может отплыть")
         }
     }
 }
+
+const val IS_DAMAGE = false
+const val IS_WEATHER_CONDITION = true
+const val CREW_COUNT_MIN = 55
+const val CREW_COUNT_MAX = 70
+const val FOOD_BOXES_COUNT = 50
