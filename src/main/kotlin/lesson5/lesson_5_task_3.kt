de02c1685f93c9bb4b0f2363d3b2532c6e25dfe3
Your Name <mrmofd@gmail.com>
2023-09-14 14:14:56 +0700

    KS-5-3 We wrote the code for the lottery

--- src/main/kotlin/lesson5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson5/lesson_5_task_3.kt
@@ -2,7 +2,7 @@ package lesson5
 
 fun main() {
 
-    println("Для выигрыша главного приза Вам нужно отгадать два числа,  введите первое число:")
+    println("Для выигрыша главного приза Вам нужно отгадать два числа (от 0 до 100),  введите первое число:")
     val firstNumb = readln().toInt()
     println("Введите второе число:")
     val secondNumb = readln().toInt()
@@ -10,10 +10,12 @@ fun main() {
     if ((firstNumb == FIRST_CORRECT_NUMB) && (secondNumb == SECOND_CORRECT_NUMB)) {
         println("Поздравляем! Вы выиграли главный приз!")
     } else if ((firstNumb == FIRST_CORRECT_NUMB) || (secondNumb == SECOND_CORRECT_NUMB))
-        println("Вы выиграли утешительный приз!")
-    else println("Неудача! Крутите барабан!")
+        println("Вы выиграли утешительный приз!\nВыигрышные числа: 17 и 9")
+    else println(
+        "Неудача! Крутите барабан!\n" +
+                "Выигрышные числа: 17 и 9"
+    )
 
-    println("Выигрышные числа: 17 и 9")
 }
 
 const val FIRST_CORRECT_NUMB = 17
