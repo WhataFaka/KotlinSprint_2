da3a4758a80728773fd50867a789dc24125430d2
Your Name <mrmofd@gmail.com>
2023-09-14 15:02:13 +0700

    KS-5-5 Added random numbers to the lottery

---
+++ src/main/kotlin/lesson5/lesson_5_task_5.kt
@@ -0,0 +1,22 @@
+package lesson5
+
+import kotlin.random.Random
+
+fun main() {
+    val correctNumb1 = Random.nextInt(101)
+    val correctNumb2 = Random.nextInt(101)
+
+    println("Для выигрыша главного приза Вам нужно отгадать два числа (от 0 до 100),  введите первое число:")
+    val firstNumb = readln().toInt()
+    println("Введите второе число:")
+    val secondNumb = readln().toInt()
+
+    if ((firstNumb == correctNumb1) && (secondNumb == correctNumb2)) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if ((firstNumb == correctNumb1) || (secondNumb == correctNumb2))
+        println("Вы выиграли утешительный приз!\nВыигрышные числа: $correctNumb1 и $correctNumb2")
+    else println(
+        "Неудача! Крутите барабан!\n" +
+                "\nВыигрышные числа: $correctNumb1 и $correctNumb2"
+    )
+}
